3c95b280fcddf54f867044af82c8634922bee0cd
modern-course-2099 <saraamiry24@gmail.com>
2020-04-20 07:30:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,13 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma("phrase")
+  if phrase.downcase
+    print "SPEAK UP"
+  else
+    print "NO, NOT SINCE 1938!"
+  end
+end
+apeak_to_grandma("Hello grandma!")
+    
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
