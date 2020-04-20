f78a8428a0ff86ae4a36d0b986fad89b7c5e8ba5
modern-course-2099 <saraamiry24@gmail.com>
2020-04-20 08:04:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,12 +7,12 @@ def speak_to_grandma(phrase)
 end
 
 apeak_to_grandma("Hello grandma!")
-    
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
