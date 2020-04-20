10664d21ee88ad26618ed0b2663ded235c72c459
modern-course-2099 <saraamiry24@gmail.com>
2020-04-20 07:41:57 +0000

    Automatically backed up by Learn

--- example.rb
+++ example.rb
@@ -1,4 +1,11 @@
-
-if 3 > 1
-puts "the value is true"
+def is_greater_than(number)
+  if number > 10
+    puts "#{number} is greater than 10"
+  else 
+    puts "#{number} is not greater than 10"
+  end
+  
 end
+
+is_greater_than(12)
+is_greater_than(8)
